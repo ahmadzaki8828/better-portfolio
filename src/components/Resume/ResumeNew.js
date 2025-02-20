9866109df0edaa5a67e9a7daf05200f41b75b60d
ahmadzaki8828 <ahmadzaki8828@gmail.com>
2025-02-20 14:20:08 -0500

    resume update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/resume.pdf"; // Ensure this path is correct
+import pdf from "../../Assets/AhmadZaki_Intern.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
