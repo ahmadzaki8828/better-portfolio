b7630e43379454551ec9f65b375335e8d5248103
Ahmad Zaki <ahmadzaki8828@gmail.com>
2025-11-21 18:03:00 -0500

    Tech Stack update

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -4,21 +4,20 @@ import {
   DiJavascript1,
   DiReact,
   DiNodejs,
-  DiMongodb,
-  DiPython,
   DiGit,
   DiJava,
 } from "react-icons/di";
 import {
   SiAmazonaws,
-  SiAmazondynamodb,
-  SiAmazons3,
   SiAngularjs,
-  SiAwsamplify,
-  SiAwslambda,
   SiNextdotjs,
   SiPostgresql,
+  SiTerraform,
   SiTypescript,
+  SiAmazondynamodb,
+  SiAmazons3,
+  SiAwslambda,
+  SiMicrosoftazure,
 } from "react-icons/si";
 import { SiTailwindcss } from "react-icons/si";
 
@@ -29,73 +28,67 @@ function Techstack() {
         <DiReact />
         <h4>React</h4>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiTypescript />
         <h4>Typescript</h4>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
         <h4>Javascript</h4>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiTailwindcss />
         <h4>Tailwind</h4>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiNextdotjs />
-        <h4>Next JS</h4>
+        <h4>Next.js</h4>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiAngularjs />
         <h4>Angular</h4>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
-        <h4>Node JS</h4>
+        <h4>Node.js</h4>
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <DiMongodb />
-        <h4>Mongo DB</h4>
-      </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
-        <h4>Postgres SQL</h4>
+        <h4>PostgreSQL</h4>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
         <h4>Git</h4>
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiFirebase />
-        <h4>Firebase</h4>
-      </Col> */}
-      <Col xs={4} md={2} className="tech-icons">
-        <DiPython />
-        <h4>Python</h4>
-      </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <DiJava />
         <h4>Java</h4>
       </Col>
+
+      {/* Cloud + Infra */}
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoftazure />
+        <h4>Azure</h4>
+      </Col>
+
       <Col xs={4} md={2} className="tech-icons">
         <SiAmazonaws />
         <h4>AWS</h4>
       </Col>
+
       <Col xs={4} md={2} className="tech-icons">
-        <SiAwsamplify />
-        <h4>AWS Amplify</h4>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAwslambda />
-        <h4>AWS Lambda</h4>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazondynamodb />
-        <h4>AWS DynamoDB</h4>
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiAmazons3 />
-        <h4>AWS S3</h4>
+        <SiTerraform />
+        <h4>Terraform</h4>
       </Col>
     </Row>
   );
